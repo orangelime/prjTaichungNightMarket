2d3b8010107f0aa3a5f929faf5a548b8bf1907b4
orangelime <gobbiby@gmail.com>
2021-12-07 16:43:43 +0800

    first commit

---
+++ prjNightMarket/Models/NightMarket.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace prjNightMarket.Models
+{
+    public class NightMarket
+    {
+        //Id編號屬性
+        public string Id { get; set; }
+        //Name名稱屬性
+        public string Name { get; set; }
+        //Address地址屬性
+        public string Address { get; set; }
+    }
+}
